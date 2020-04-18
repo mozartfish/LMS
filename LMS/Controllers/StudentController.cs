e4e791ac60d8ab43943b78337c5400a5d98f2d0b
Andrew Dron <adron27@gmail.com>
2020-04-18 17:27:58 -0600

    started student controller

--- LMS/Controllers/StudentController.cs
+++ LMS/Controllers/StudentController.cs
@@ -7,154 +7,195 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LMS.Controllers
 {
-  [Authorize(Roles = "Student")]
-  public class StudentController : CommonController
-  {
-
-    public IActionResult Index()
+    [Authorize(Roles = "Student")]
+    public class StudentController : CommonController
     {
-      return View();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Catalog()
+        {
+            return View();
+        }
+
+        public IActionResult Class(string subject, string num, string season, string year)
+        {
+            ViewData["subject"] = subject;
+            ViewData["num"] = num;
+            ViewData["season"] = season;
+            ViewData["year"] = year;
+            return View();
+        }
+
+        public IActionResult Assignment(string subject, string num, string season, string year, string cat, string aname)
+        {
+            ViewData["subject"] = subject;
+            ViewData["num"] = num;
+            ViewData["season"] = season;
+            ViewData["year"] = year;
+            ViewData["cat"] = cat;
+            ViewData["aname"] = aname;
+            return View();
+        }
+
+
+        public IActionResult ClassListings(string subject, string num)
+        {
+            System.Diagnostics.Debug.WriteLine(subject + num);
+            ViewData["subject"] = subject;
+            ViewData["num"] = num;
+            return View();
+        }
+
+
+        /*******Begin code to modify********/
+
+        /// <summary>
+        /// Returns a JSON array of the classes the given student is enrolled in.
+        /// Each object in the array should have the following fields:
+        /// "subject" - The subject abbreviation of the class (such as "CS")
+        /// "number" - The course number (such as 5530)
+        /// "name" - The course name
+        /// "season" - The season part of the semester
+        /// "year" - The year part of the semester
+        /// "grade" - The grade earned in the class, or "--" if one hasn't been assigned
+        /// </summary>
+        /// <param name="uid">The uid of the student</param>
+        /// <returns>The JSON array</returns>
+        public IActionResult GetMyClasses(string uid)
+        {
+            var query = from s in db.Students
+                        where s.UId == uid
+                        join en in db.EnrollmentGrade on s.UId equals en.UId into join1
+
+                        from j1 in join1
+                        join klasse in db.Classes on j1.ClassId equals klasse.ClassId into join2
+
+                        from j2 in join2
+                        join c in db.Courses on j2.CourseId equals c.CourseId into join3
+
+                        from j3 in join3
+                        select new
+                        {
+                            subject = j3.DeptAbbreviation,
+                            number = j3.CourseNumber,
+                            name = j3.CourseName,
+                            season = j2.Season,
+                            year = j2.Year,
+                            grade = j1.Grade ?? "--"
+                        };
+
+            return Json(query.ToArray());
+        }
+
+        /// <summary>
+        /// Returns a JSON array of all the assignments in the given class that the given student is enrolled in.
+        /// Each object in the array should have the following fields:
+        /// "aname" - The assignment name
+        /// "cname" - The category name that the assignment belongs to
+        /// "due" - The due Date/Time
+        /// "score" - The score earned by the student, or null if the student has not submitted to this assignment.
+        /// </summary>
+        /// <param name="subject">The course subject abbreviation</param>
+        /// <param name="num">The course number</param>
+        /// <param name="season">The season part of the semester for the class the assignment belongs to</param>
+        /// <param name="year">The year part of the semester for the class the assignment belongs to</param>
+        /// <param name="uid"></param>
+        /// <returns>The JSON array</returns>
+        public IActionResult GetAssignmentsInClass(string subject, int num, string season, int year, string uid)
+        {
+            var query = from c in db.Courses
+                        where c.DeptAbbreviation == subject && c.CourseNumber == num
+                        join klasse in db.Classes on c.CourseId equals klasse.ClassId into join1
+
+                        from j1 in join1
+                        join ac in db.AssignmentCategories on j1.ClassId equals ac.ClassId into join2
+
+                        from j2 in join2
+                        join assign in db.Assignments on j2.CategoryId equals assign.CategoryId into join3
+
+                        from j3 in join3
+                        join sub in db.Submission on j3.AssignmentId equals sub.AssignmentId into join4
+
+                        from j4 in join4
+                        select new
+                        {
+                            aname = j3.AsgmtName,
+                            cname = j2.CategoryName,
+                            due = j3.DueDate,
+                            score = j4.Score
+                        };
+
+            return Json(query.ToArray());
+        }
+
+
+
+        /// <summary>
+        /// Adds a submission to the given assignment for the given student
+        /// The submission should use the current time as its DateTime
+        /// You can get the current time with DateTime.Now
+        /// The score of the submission should start as 0 until a Professor grades it
+        /// If a Student submits to an assignment again, it should replace the submission contents
+        /// and the submission time (the score should remain the same).
+        /// Does *not* automatically reject late submissions.
+        /// </summary>
+        /// <param name="subject">The course subject abbreviation</param>
+        /// <param name="num">The course number</param>
+        /// <param name="season">The season part of the semester for the class the assignment belongs to</param>
+        /// <param name="year">The year part of the semester for the class the assignment belongs to</param>
+        /// <param name="category">The name of the assignment category in the class</param>
+        /// <param name="asgname">The new assignment name</param>
+        /// <param name="uid">The student submitting the assignment</param>
+        /// <param name="contents">The text contents of the student's submission</param>
+        /// <returns>A JSON object containing {success = true/false}.</returns>
+        public IActionResult SubmitAssignmentText(string subject, int num, string season, int year,
+          string category, string asgname, string uid, string contents)
+        {
+
+            return Json(new { success = false });
+        }
+
+
+        /// <summary>
+        /// Enrolls a student in a class.
+        /// </summary>
+        /// <param name="subject">The department subject abbreviation</param>
+        /// <param name="num">The course number</param>
+        /// <param name="season">The season part of the semester</param>
+        /// <param name="year">The year part of the semester</param>
+        /// <param name="uid">The uid of the student</param>
+        /// <returns>A JSON object containing {success = {true/false},
+        /// false if the student is already enrolled in the Class.</returns>
+        public IActionResult Enroll(string subject, int num, string season, int year, string uid)
+        {
+
+            return Json(new { success = false });
+        }
+
+
+
+        /// <summary>
+        /// Calculates a student's GPA
+        /// A student's GPA is determined by the grade-point representation of the average grade in all their classes.
+        /// Assume all classes are 4 credit hours.
+        /// If a student does not have a grade in a class ("--"), that class is not counted in the average.
+        /// If a student does not have any grades, they have a GPA of 0.0.
+        /// Otherwise, the point-value of a letter grade is determined by the table on this page:
+        /// https://advising.utah.edu/academic-standards/gpa-calculator-new.php
+        /// </summary>
+        /// <param name="uid">The uid of the student</param>
+        /// <returns>A JSON object containing a single field called "gpa" with the number value</returns>
+        public IActionResult GetGPA(string uid)
+        {
+
+            return Json(null);
+        }
+
+        /*******End code to modify********/
+
     }
-
-    public IActionResult Catalog()
-    {
-      return View();
-    }
-
-    public IActionResult Class(string subject, string num, string season, string year)
-    {
-      ViewData["subject"] = subject;
-      ViewData["num"] = num;
-      ViewData["season"] = season;
-      ViewData["year"] = year;
-      return View();
-    }
-
-    public IActionResult Assignment(string subject, string num, string season, string year, string cat, string aname)
-    {
-      ViewData["subject"] = subject;
-      ViewData["num"] = num;
-      ViewData["season"] = season;
-      ViewData["year"] = year;
-      ViewData["cat"] = cat;
-      ViewData["aname"] = aname;
-      return View();
-    }
-
-
-    public IActionResult ClassListings(string subject, string num)
-    {
-      System.Diagnostics.Debug.WriteLine(subject + num);
-      ViewData["subject"] = subject;
-      ViewData["num"] = num;
-      return View();
-    }
-
-
-    /*******Begin code to modify********/
-
-    /// <summary>
-    /// Returns a JSON array of the classes the given student is enrolled in.
-    /// Each object in the array should have the following fields:
-    /// "subject" - The subject abbreviation of the class (such as "CS")
-    /// "number" - The course number (such as 5530)
-    /// "name" - The course name
-    /// "season" - The season part of the semester
-    /// "year" - The year part of the semester
-    /// "grade" - The grade earned in the class, or "--" if one hasn't been assigned
-    /// </summary>
-    /// <param name="uid">The uid of the student</param>
-    /// <returns>The JSON array</returns>
-    public IActionResult GetMyClasses(string uid)
-    {
-   
-      return Json(null);
-    }
-
-    /// <summary>
-    /// Returns a JSON array of all the assignments in the given class that the given student is enrolled in.
-    /// Each object in the array should have the following fields:
-    /// "aname" - The assignment name
-    /// "cname" - The category name that the assignment belongs to
-    /// "due" - The due Date/Time
-    /// "score" - The score earned by the student, or null if the student has not submitted to this assignment.
-    /// </summary>
-    /// <param name="subject">The course subject abbreviation</param>
-    /// <param name="num">The course number</param>
-    /// <param name="season">The season part of the semester for the class the assignment belongs to</param>
-    /// <param name="year">The year part of the semester for the class the assignment belongs to</param>
-    /// <param name="uid"></param>
-    /// <returns>The JSON array</returns>
-    public IActionResult GetAssignmentsInClass(string subject, int num, string season, int year, string uid)
-    {     
-
-      return Json(null);
-    }
-
-
-
-    /// <summary>
-    /// Adds a submission to the given assignment for the given student
-    /// The submission should use the current time as its DateTime
-    /// You can get the current time with DateTime.Now
-    /// The score of the submission should start as 0 until a Professor grades it
-    /// If a Student submits to an assignment again, it should replace the submission contents
-    /// and the submission time (the score should remain the same).
-	/// Does *not* automatically reject late submissions.
-    /// </summary>
-    /// <param name="subject">The course subject abbreviation</param>
-    /// <param name="num">The course number</param>
-    /// <param name="season">The season part of the semester for the class the assignment belongs to</param>
-    /// <param name="year">The year part of the semester for the class the assignment belongs to</param>
-    /// <param name="category">The name of the assignment category in the class</param>
-    /// <param name="asgname">The new assignment name</param>
-    /// <param name="uid">The student submitting the assignment</param>
-    /// <param name="contents">The text contents of the student's submission</param>
-    /// <returns>A JSON object containing {success = true/false}.</returns>
-    public IActionResult SubmitAssignmentText(string subject, int num, string season, int year, 
-      string category, string asgname, string uid, string contents)
-    {
-     
-      return Json(new { success = false });
-    }
-
-    
-    /// <summary>
-    /// Enrolls a student in a class.
-    /// </summary>
-    /// <param name="subject">The department subject abbreviation</param>
-    /// <param name="num">The course number</param>
-    /// <param name="season">The season part of the semester</param>
-    /// <param name="year">The year part of the semester</param>
-    /// <param name="uid">The uid of the student</param>
-    /// <returns>A JSON object containing {success = {true/false},
-	/// false if the student is already enrolled in the Class.</returns>
-    public IActionResult Enroll(string subject, int num, string season, int year, string uid)
-    {      
-
-      return Json(new { success = false });
-    }
-
-
-
-    /// <summary>
-    /// Calculates a student's GPA
-    /// A student's GPA is determined by the grade-point representation of the average grade in all their classes.
-    /// Assume all classes are 4 credit hours.
-    /// If a student does not have a grade in a class ("--"), that class is not counted in the average.
-    /// If a student does not have any grades, they have a GPA of 0.0.
-    /// Otherwise, the point-value of a letter grade is determined by the table on this page:
-    /// https://advising.utah.edu/academic-standards/gpa-calculator-new.php
-    /// </summary>
-    /// <param name="uid">The uid of the student</param>
-    /// <returns>A JSON object containing a single field called "gpa" with the number value</returns>
-    public IActionResult GetGPA(string uid)
-    {     
-
-      return Json(null);
-    }
-
-    /*******End code to modify********/
-
-  }
 }
